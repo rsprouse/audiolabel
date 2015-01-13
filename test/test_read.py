0828a602b4f9de3de33a962d65ad00f140a5ed73
Ronald Sprouse <ronald@berkeley.edu>
2015-01-13 15:43:10 -0800

    Fixes for new API.

--- test/test_read.py
+++ test/test_read.py
@@ -3,17 +3,17 @@ import audiolabel
 # Test reading of a Praat long TextGrid.
 def test_praat_long():
     lm = audiolabel.LabelManager(
-        fromFile='test/this_is_a_label_file.long.TextGrid',
-        fromType='praat_long'
+        from_file='test/this_is_a_label_file.long.TextGrid',
+        from_type='praat_long'
     )
     assert len(lm._tiers) == 3
     assert lm.names() == ('word', 'phone', 'stimulus')
 
-# Test reading of a Praat long TextGrid with generic 'praat' fromType.
+# Test reading of a Praat long TextGrid with generic 'praat' from_type.
 def test_praat_long_generic_fromtype():
     lm = audiolabel.LabelManager(
-        fromFile='test/this_is_a_label_file.long.TextGrid',
-        fromType='praat'
+        from_file='test/this_is_a_label_file.long.TextGrid',
+        from_type='praat'
     )
     assert len(lm._tiers) == 3
     assert lm.names() == ('word', 'phone', 'stimulus')
@@ -21,44 +21,53 @@ def test_praat_long_generic_fromtype():
 # Test reading of a Praat short TextGrid.
 def test_praat_short():
     lm = audiolabel.LabelManager(
-        fromFile='test/this_is_a_label_file.short.TextGrid',
-        fromType='praat_short'
+        from_file='test/this_is_a_label_file.short.TextGrid',
+        from_type='praat_short'
     )
     assert len(lm._tiers) == 3
     assert lm.names() == ('word', 'phone', 'stimulus')
 
-# Test reading of a Praat short TextGrid with generic 'praat' fromType.
+# Test reading of a Praat short TextGrid with generic 'praat' from_type.
 def test_praat_short_generic_fromtype():
     lm = audiolabel.LabelManager(
-        fromFile='test/this_is_a_label_file.short.TextGrid',
-        fromType='praat'
+        from_file='test/this_is_a_label_file.short.TextGrid',
+        from_type='praat'
     )
 
 def test_praat_utf_8():
     lm = audiolabel.LabelManager(
-        fromFile='test/ipa.TextGrid',
-        fromType='praat'
+        from_file='test/ipa.TextGrid',
+        from_type='praat'
     )
 
 def test_praat_utf_16_be():
     lm = audiolabel.LabelManager(
-        fromFile='test/Turkmen_NA_20130919_G_3.TextGrid',
-        fromType='praat'
+        from_file='test/Turkmen_NA_20130919_G_3.TextGrid',
+        from_type='praat'
     )
 
 def test_esps():
     lm = audiolabel.LabelManager(
-        fromFile='test/sample.esps',
-        fromType='esps'
+        from_file='test/sample.esps',
+        from_type='esps'
     )
 
 def test_get_praat_header():
     lm = audiolabel.LabelManager(
-        fromFile='test/this_is_a_label_file.long.TextGrid',
-        fromType='praat'
+        from_file='test/this_is_a_label_file.long.TextGrid',
+        from_type='praat'
     )
     assert len(lm._tiers) == 3
     assert lm.names() == ('word', 'phone', 'stimulus')
     #lm._getPraatHeader()
 
 
+if __name__ == '__main__':
+    test_praat_long()
+    test_praat_long_generic_fromtype()
+    test_praat_short()
+    test_praat_short_generic_fromtype()
+    test_praat_utf_8()
+    test_praat_utf_16_be()
+    test_esps()
+    test_get_praat_header()
