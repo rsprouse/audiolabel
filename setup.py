b11781ae2cad5c86afc572abd74c09488139e7f5
Ronald Sprouse <ronald@berkeley.edu>
2015-01-20 11:39:50 -0800

    Add audiolabel_update_api script.

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ from distutils.core import setup
 setup(
   name = 'audiolabel',
   #version='0.1',
-  py_modules=['audiolabel']
+  py_modules=['audiolabel'],
+  scripts = ['audiolabel_update_api']
 )
