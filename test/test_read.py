c583e90bcb9c3e7db93e284ac2c97fe969363855
Ronald Sprouse <ronald@berkeley.edu>
2015-01-20 10:47:47 -0800

    Update to new api. Add test_table().

--- test/test_read.py
+++ test/test_read.py
@@ -7,7 +7,7 @@ def test_praat_long():
         from_type='praat_long'
     )
     assert len(lm._tiers) == 3
-    assert lm.names() == ('word', 'phone', 'stimulus')
+    assert lm.names == ('word', 'phone', 'stimulus')
 
 # Test reading of a Praat long TextGrid with generic 'praat' from_type.
 def test_praat_long_generic_fromtype():
@@ -16,7 +16,7 @@ def test_praat_long_generic_fromtype():
         from_type='praat'
     )
     assert len(lm._tiers) == 3
-    assert lm.names() == ('word', 'phone', 'stimulus')
+    assert lm.names == ('word', 'phone', 'stimulus')
 
 # Test reading of a Praat short TextGrid.
 def test_praat_short():
@@ -25,7 +25,7 @@ def test_praat_short():
         from_type='praat_short'
     )
     assert len(lm._tiers) == 3
-    assert lm.names() == ('word', 'phone', 'stimulus')
+    assert lm.names == ('word', 'phone', 'stimulus')
 
 # Test reading of a Praat short TextGrid with generic 'praat' from_type.
 def test_praat_short_generic_fromtype():
@@ -51,6 +51,22 @@ def test_esps():
         from_file='test/sample.esps',
         from_type='esps'
     )
+    assert len(lm._tiers) == 2
+    assert lm.tier(0)[2].text == 'eh'
+    assert lm.tier(1)[0].text == 'sat'
+
+def test_table():
+    lm = audiolabel.LabelManager(
+        from_file='test/sample.table',
+        from_type='table',
+        fields_in_head=True,
+        t1_col='sec'
+    )
+    assert len(lm._tiers) == 6
+    assert lm.tier('rms')[0].text == '7.1'
+    rms0 = lm.tier('rms')[0]
+    assert lm.tier('rms').next(rms0).text == '7.4'
+    assert lm.tier('rms').next(rms0, 2).text == '7.3'
 
 def test_get_praat_header():
     lm = audiolabel.LabelManager(
@@ -58,8 +74,7 @@ def test_get_praat_header():
         from_type='praat'
     )
     assert len(lm._tiers) == 3
-    assert lm.names() == ('word', 'phone', 'stimulus')
-    #lm._getPraatHeader()
+    assert lm.names == ('word', 'phone', 'stimulus')
 
 
 if __name__ == '__main__':
@@ -70,4 +85,5 @@ if __name__ == '__main__':
     test_praat_utf_8()
     test_praat_utf_16_be()
     test_esps()
+    test_table()
     test_get_praat_header()
