8e77d9a7298fbc4050a6b7a72c9e0264f4a8ea36
Ronald Sprouse <ronald@berkeley.edu>
2015-01-20 11:37:47 -0800

    Add initialization test.

--- test/test_read.py
+++ test/test_read.py
@@ -1,4 +1,29 @@
+#!/usr/bin/env python
+# -*- vim: set fileencoding=utf-8 -*-
+
 import audiolabel
+
+def test_initialization():
+    l1 = audiolabel.Label('first', 1.0, 2.0)
+    l2 = audiolabel.Label('second', 2.0, 3.0)
+    l3 = audiolabel.Label('third', 3.0, 4.0)
+    assert l1.text == 'first'
+    assert l1.t1 == 1.0
+    assert l1.t2 == 2.0
+    t1 = audiolabel.IntervalTier(name='tier1')
+    t1.add(l1)
+    t1.add(l2)
+    t1.add(l3)
+    assert t1.label_at(2.5) == l2
+    assert t1[0] == l1
+    assert t1[-1] == l3
+    lm = audiolabel.LabelManager()
+    lm.add(t1)
+    assert lm.tier('tier1') == t1
+    assert t1.next(l1) == l2
+    assert t1.next(l1, skip=1) == l3
+    assert t1.prev(l3) == l2
+    assert t1.prev(l3, skip=1) == l1
 
 # Test reading of a Praat long TextGrid.
 def test_praat_long():
@@ -33,18 +58,23 @@ def test_praat_short_generic_fromtype():
         from_file='test/this_is_a_label_file.short.TextGrid',
         from_type='praat'
     )
+    assert len(lm._tiers) == 3
+    assert lm.names == ('word', 'phone', 'stimulus')
 
 def test_praat_utf_8():
     lm = audiolabel.LabelManager(
         from_file='test/ipa.TextGrid',
         from_type='praat'
     )
+    assert lm.tier('phone').label_at(0.6).text == u"ɯ"
+    assert lm.tier('phone').label_at(0.7).text == u"ʤ"
 
 def test_praat_utf_16_be():
     lm = audiolabel.LabelManager(
         from_file='test/Turkmen_NA_20130919_G_3.TextGrid',
         from_type='praat'
     )
+    # TODO: add test
 
 def test_esps():
     lm = audiolabel.LabelManager(
@@ -78,6 +108,7 @@ def test_get_praat_header():
 
 
 if __name__ == '__main__':
+    test_initialization()
     test_praat_long()
     test_praat_long_generic_fromtype()
     test_praat_short()
