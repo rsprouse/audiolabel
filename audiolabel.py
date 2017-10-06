98c291be61eff80c3cd0901b44fa860d2b510ac7
BCE <bce@berkeley.edu>
2017-10-05 18:43:55 -0700

    Append list of dataframes.

--- audiolabel.py
+++ audiolabel.py
@@ -656,7 +656,7 @@ ignore_index = boolean; value is passed to pd.concat()'s ignore_index
             if inc in includes:
                 asdf_includes.append(inc)
 
-        dfs = None
+        dflist = None
         for fidx, f in enumerate(fname):
             dirname, basename = os.path.split(f)
             barename, ext = os.path.splitext(basename)
@@ -675,8 +675,8 @@ ignore_index = boolean; value is passed to pd.concat()'s ignore_index
             try:
                 lm = LabelManager(from_file=f, from_type=ftype, codec=codec)
                 tlist = lm.as_df(tiers=tiers, includes=asdf_includes)
-                if dfs is None:
-                    dfs = [pd.DataFrame() for t in tlist]
+                if dflist is None:
+                    dflist = [[] for t in tlist] # list of lists
             except Exception as e:
                 if stop_on_error is True:
                     raise e
@@ -684,26 +684,27 @@ ignore_index = boolean; value is passed to pd.concat()'s ignore_index
                     sys.stderr.write(e.msg())
                     continue
             for idx, tr in enumerate(tlist):
+                assigndict = {}
                 if barename_re is not None:
-                    tr = tr.assign(**m.groupdict())
-                incldict = {}
+                    assigndict = m.groupdict().copy()
                 for inc in includes:
                     if inc == 'barename':
-                        incldict['barename'] = barename
+                        assigndict['barename'] = barename
                     elif inc == 'fname':
-                        incldict['fname'] = f
+                        assigndict['fname'] = f
                     elif inc == 'dirname':
-                        incldict['dirname'] = dirname
+                        assigndict['dirname'] = dirname
                     elif inc == 'fidx':
-                        incldict['fidx'] = fidx
+                        assigndict['fidx'] = fidx
                     elif inc == 'extension':
-                        incldict['extension'] = ext
+                        assigndict['extension'] = ext
                     elif inc == 'dur':
-                        incldict['dur'] = tr.t2 - tr.t1
+                        assigndict['dur'] = tr.t2 - tr.t1
                     elif inc == 'midpt':
-                        incldict['midpt'] = (tr.t1 + tr.t2) / 2
-                tr = tr.assign(**incldict)
-                dfs[idx] = pd.concat([dfs[idx], tr], ignore_index=ignore_index)
+                        assigndict['midpt'] = (tr.t1 + tr.t2) / 2
+                tr = tr.assign(**assigndict)
+                dflist[idx].append(tr)
+        dfs = [pd.concat(lst, ignore_index=ignore_index) for lst in dflist]
         [df.rename(columns={'text': 'label'}, inplace=True) for df in dfs]
         return dfs
  
