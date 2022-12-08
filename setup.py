e7d3094399d1db04340660c7f0e7bf9c533aa07b
Ronald Sprouse <ronald@berkeley.edu>
2022-12-08 12:33:47 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
   name = 'audiolabel',
-  #version='0.2.0',
+  version='0.2.1',
   py_modules = ['audiolabel'],
   scripts = ['audiolabel_update_api'],
   classifiers = [
